7bf3a9e4a346464a009f6075afed83212fa0d4bc
Henrik Karlsson <hk.henrik@gmail.com>
2015-03-31 01:12:56 +0200

    GGen

--- server/app/server.js
+++ server/app/server.js
@@ -13,36 +13,47 @@ console.log('generating');
 var galaxy = new Galaxy();
 console.log('done generating');
 
-let galaxyStream = fs.createWriteStream('world.galaxy');
+let writeStream = fs.createWriteStream(__dirname + '/world' + (+new Date()) + '.galaxy');
 
-for (let i = 0; i < galaxy.stars.length; i++) {
-    let star = galaxy.stars[i];
+var i = 0;
+function writeStars() {
+    var cont = true;
+    while (cont && i < galaxy.stars.length) {
+        let star = galaxy.stars[i];
 
-    // Color int (4), Size double (8), Poisiton double (8 * 3), NumPlanets int (4)
-    var starBuffer = new Buffer(4 + 8 + 8 * 3 + 4);
-    starBuffer.writeInt32BE(star.color);
-    starBuffer.writeDoubleBE(star.size);
-    starBuffer.writeDoubleBE(star.position.x);
-    starBuffer.writeDoubleBE(star.position.y);
-    starBuffer.writeDoubleBE(star.position.z);
-    starBuffer.writeInt8(star.planets.length);
-    galaxyStream.write(starBuffer);
+        // Color int (4), Size double (8), Poisiton double (8 * 3), NumPlanets byte (1)
+        var starBuffer = new Buffer(4 + 8 + 8 * 3 + 1);
+        starBuffer.writeUInt32BE(star.color, 0);
+        starBuffer.writeDoubleBE(star.size, 4);
+        starBuffer.writeDoubleBE(star.position.x, 12);
+        starBuffer.writeDoubleBE(star.position.y, 20);
+        starBuffer.writeDoubleBE(star.position.z, 28);
+        starBuffer.writeUInt8(star.planets.length, 36);
+        writeStream.write(starBuffer);
+        var innerCont = true;
 
-    for (let j = 0; j < star.planets.length; j++) {
-        let planet = star.planets[j];
+        for (let j = 0; j < star.planets.length; j++) {
+            let planet = star.planets[j];
 
-        // Distance double (8), Size dobule (8)
-        var planetBuffer = new Buffer(8 + 8);
-        planetBuffer.writeDoubleBE(planet.distance);
-        planetBuffer.writeDoubleBE(planet.size);
-        galaxyStream.write(planetBuffer);
+            // Distance double (8), Size dobule (8)
+            var planetBuffer = new Buffer(8 + 8);
+            planetBuffer.writeDoubleBE(planet.distance, 0);
+            planetBuffer.writeDoubleBE(planet.size, 8);
+            innerCont = writeStream.write(planetBuffer);
+        }
+
+        cont = innerCont;
+        i++;
     }
-
-    galaxyStream.uncork()
+    if (i < galaxy.stars.length) {
+        writeStream.needDrain = true;
+        writeStream.once('drain', writeStars);
+    } else {
+        writeStream.end();
+        console.log('File written (' + i + ' stars)');
+    }
 }
-
-galaxyStream.end();
-console.log('File writteen');
+writeStars();
 
 
 
