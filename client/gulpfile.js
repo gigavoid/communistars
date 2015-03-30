f7a3fc56230f08925ac00895f470df9000389392
Henrik Karlsson <hk.henrik@gmail.com>
2015-03-30 16:25:28 +0200

    New autoreload method

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     stylus = require('gulp-stylus'),
     jade = require('gulp-jade'),
     sourcemaps = require('gulp-sourcemaps'),
-    livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync'),
     plumber = require('gulp-plumber'),
     gutil = require('gulp-util'),
     concat = require('gulp-concat'),
@@ -35,8 +35,7 @@ gulp.task('styles', function() {
             compress: true
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./out/static/'))
-        .pipe(livereload());
+        .pipe(gulp.dest('./out/static/'));
 });
 
 var browserifyOpts = {
@@ -53,6 +52,7 @@ gulp.task('scripts', function() {
     var init = bundler.bundle()
 		.on('error', function(err){
             gutil.log(err.message);
+            browserSync.notify('<font color="red">Error compiling the script, please check your console</font>', 5000);
             this.emit('end');
 		})
 	    .pipe(plumber(exitIfNotWatching))
@@ -65,32 +65,38 @@ gulp.task('scripts', function() {
             .pipe(uglify())
             .pipe(sourcemaps.write('.'));
     }
-    return init.pipe(gulp.dest('./out/static/'))
-        .pipe(livereload());
+
+    browserSync.notify('Script compiled');
+    return init.pipe(gulp.dest('./out/static/'));
 });
 
 gulp.task('static', function() {
     return gulp.src('./app/static/**')
-        .pipe(gulp.dest('./out/static/'))
-        .pipe(livereload());
+        .pipe(gulp.dest('./out/static/'));
 });
 
 gulp.task('templates', function() {
 	return gulp.src('./app/templates/*.jade')
 		.pipe(plumber(exitIfNotWatching))
 		.pipe(jade())
-		.pipe(gulp.dest('./out/'))
-		.pipe(livereload());
+		.pipe(gulp.dest('./out/'));
 });
 
 gulp.task('watch', function() {
 	watching = true;
 	gulp.start(['styles', 'templates', 'scripts', 'static']);
-	livereload.listen();
+
 	gulp.watch('./app/styl/*', ['styles']);
 	gulp.watch('./app/templates/*', ['templates']);
 	gulp.watch('./app/scripts/*', ['scripts']);
 	gulp.watch('./app/static/*', ['static']);
+
+    browserSync({
+        server: {
+            baseDir: './out/'
+        },
+        open: false
+    });
 });
 
 gulp.task('default', ['styles', 'templates', 'scripts', 'static'], function() {
